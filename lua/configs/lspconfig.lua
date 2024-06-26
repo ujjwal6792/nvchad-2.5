4b4ea669dcf91f8475fc3324934f323a477ab875
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2024-06-27 02:23:38 +0530

    fix: remove filetypes for tailwind lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -69,9 +69,9 @@ require("lspconfig").svelte.setup {
 lspconfig.tailwindcss.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {
-    "astro, astro-markdown, gohtml, gohtmltmpl, html, html-eex, markdown, mdx, css, less, postcss, sass, scss, stylus, javascript, javascriptreact, typescript, typescriptreact, vue, svelte",
-  },
+  -- filetypes = {
+  --   "astro, astro-markdown, gohtml, gohtmltmpl, html, html-eex, markdown, mdx, css, less, postcss, sass, scss, stylus, javascript, javascriptreact, typescript, typescriptreact, vue, svelte",
+  -- },
 }
 lspconfig.astro.setup {
   init_options = {
