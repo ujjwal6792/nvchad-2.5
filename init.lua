b8106051b48c72fa4f687e5035c92117f1a07760
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2025-04-29 15:05:12 +0530

    feat: add a dump function to print lua

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+_G.dump = function(...)
+  print(vim.inspect(...))
+end
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
